84a388c0180fdf09b1a003eb42217cdb8e95dde0
sabitalizade <sabitalizade@gmail.com>
2021-02-10 11:20:44 +0400

    any

--- app.js
+++ app.js
@@ -1,76 +1,67 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import http from "http"
-import {Server} from "socket.io"
-
+import http from "http";
+import { Server } from "socket.io";
 
 //app config
-const app=express();
-const dbConnection="mongodb+srv://admin:admin@chat.jhrtz.mongodb.net/chat?retryWrites=true&w=majority"
-const PORT =process.env.PORT || 5050
+const app = express();
+const dbConnection =
+  "mongodb+srv://admin:admin@chat.jhrtz.mongodb.net/chat?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5050;
 
 const server = http.createServer(app);
 
-
 const io = new Server(server, {
-    cors: {
-        // origin: "https://sasocket.vercel.app/",
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-      }
-  });
-
-
+  cors: {
+    // origin: "https://sasocket.vercel.app",
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 //db config
 
-mongoose.connect(dbConnection,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+mongoose.connect(dbConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
 
-const db = mongoose.connection
+db.once("open", () => {
+  console.log("db connected");
+  const dbCollection = db.collection("messages");
+  const msgstrem = dbCollection.watch();
 
-db.once("open",()=>{
-    console.log("db connected")
-const dbCollection=db.collection("messages")
-const msgstrem= dbCollection.watch()
-
-msgstrem.on("change",change=>{
-    if(change.operationType=="insert"){
-        const data=change.fullDocument
-        console.log(data)
-        io.emit("msg", data)
- 
-
-    }else{
-        console.log("error")
+  msgstrem.on("change", (change) => {
+    if (change.operationType == "insert") {
+      const data = change.fullDocument;
+      console.log(data);
+      io.emit("msg", data);
+    } else {
+      console.log("error");
     }
-})
-
-})
+  });
+});
 
 // midleware
 app.use(cors());
 app.use(express.json());
 
+///request
+import userRouter from "./router/userRouter.js";
+import messageRouter from "./router/messageRouter.js";
 
-///request 
-import userRouter from "./router/userRouter.js"
-import messageRouter from "./router/messageRouter.js"
+app.get("/", (req, res) => {
+  res.send("fuck you");
+});
 
-app.get("/",(req,res)=>{
-    res.send("fuck you")
-})
+app.use("/", userRouter);
+app.use("/", messageRouter);
 
-app.use("/",userRouter)
-app.use("/",messageRouter)
+//listen
 
-//listen 
-
-
-server.listen(PORT,()=>{
-    console.log(`App start http://localhost:${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`App start http://localhost:${PORT}`);
+});
